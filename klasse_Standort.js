82b91e8251bc239baf622bd695bbc197880fc33a
Felix Soehn <soehnf@gmail.com>
2021-05-31 18:02:59 +0200

    Co-authored-by: Cara0604 <Cara0604@users.noreply.github.com>

--- klasse_Standort.js
+++ klasse_Standort.js
@@ -1,36 +1,3 @@
-class Standort{
-    constructor(la, lo, name, type, adress, bild, creditor, division, partner_since_year, purchasing_volume, estimated_leverage, employees_female, employees_male, audit_type, fair_wear_audit,
-        last_fair_wear_training, bsci_id, wrap_id, complaints, certificates, mode_of_transportation, port_name, port_coordinates_latitude, port_coordinates_longitude, warehouse_name, carbon_footprint) {
 
-    this.la = la;
-    this.lo = lo;
-    this.name = name;
-    this.type = type;
-    this.adress = adress;
-    this.bild = bild;
-    this.creditor = creditor;
-    this.division = division;
-    this.partner_since_year = partner_since_year;
-    this.purchasing_volume = purchasing_volume;
-    this.estimated_leverage = estimated_leverage;
-    this.employees_female = employees_female;
-    this.employees_male = employees_male;
-    this.audit_type = audit_type;
-    this.fair_wear_audit = fair_wear_audit;
-    this.last_fair_wear_training = last_fair_wear_training;
-    this.sci_id = bsci_id;
-    this.wrap_id = wrap_id;
-    this.complaints = complaints;
-    this.certificates = certificates;
-    this.mode_of_transportation = mode_of_transportation;
-    this.port_name = port_name;
-    this.port_coordinates_latitude = port_coordinates_latitude;
-    this.port_coordinates_longitude = port_coordinates_latitude;
-    this.warehouse_name = warehouse_name;
-    this.carbon_footprint = carbon_footprint;
-    }
+   
 
-    get_name() {
-        return this.name;
-    }
-}   
--- main.js
+++ main.js
@@ -1,3 +1,54 @@
+/* Klasse Standort */
+
+class Standort{
+  /* Konstruktor der Klasse */
+  constructor(la, lo, name, type, adress, bild, creditor, division, partner_since_year, purchasing_volume,
+     estimated_leverage, employees_female, employees_male, audit_type, fair_wear_audit,
+      last_fair_wear_training, bsci_id, wrap_id, complaints, certificates, mode_of_transportation, 
+      port_name, port_coordinates_latitude, port_coordinates_longitude, warehouse_name, carbon_footprint, street, street_addition,place,country_code, country) {
+
+  this.la = la;
+  this.getLa = function(){
+      return this.la;
+  }
+  this.lo = lo;
+  this.getLo = function() {
+      return this.lo;
+  }
+  this.name = name;
+  this.type = type;
+  this.adress = adress;
+  this.bild = bild;
+  this.creditor = creditor;
+  this.division = division;
+  this.partner_since_year = partner_since_year;
+  this.purchasing_volume = purchasing_volume;
+  this.estimated_leverage = estimated_leverage;
+  this.employees_female = employees_female;
+  this.employees_male = employees_male;
+  this.audit_type = audit_type;
+  this.fair_wear_audit = fair_wear_audit;
+  this.last_fair_wear_training = last_fair_wear_training;
+  this.sci_id = bsci_id;
+  this.wrap_id = wrap_id;
+  this.complaints = complaints;
+  this.certificates = certificates;
+  this.mode_of_transportation = mode_of_transportation;
+  this.port_name = port_name;
+  this.port_coordinates_latitude = port_coordinates_latitude;
+  this.port_coordinates_longitude = port_coordinates_latitude;
+  this.warehouse_name = warehouse_name;
+  this.carbon_footprint = carbon_footprint;
+  this.street =street;
+  this.street_addition = street_addition;
+  this.place = place;
+  this.country_code = country_code;
+  this.country = country;
+  }
+
+} 
+
+/* Globale Variablen */
 var la;
 var lo;
 var name;
@@ -31,199 +82,130 @@ var port_coordinates_longitude;
 var warehouse_name;
 var carbon_footprint;
 
+var testo = [];
+var contentString = [];
+
+var json_length;
+
 var markers = [];
 var markers2 = [];
 
-
+/* Request um Daten zu holen */
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     var myObj = JSON.parse(this.responseText);
-    la = myObj.results[1].data.coordinates.latitude;
-    lo = myObj.results[1].data.coordinates.longitude;
-    name = myObj.results[1].data.name;
-    type = myObj.results[1].data.type;
-    street = myObj.results[1].data.street;
-    street_addition = myObj.results[1].data.street_addition;
-    zip_code = myObj.results[1].data.zip_code;
-    place = myObj.results[1].data.place;
-    country_code = myObj.results[1].data.country_code;
-    country = myObj.results[1].data.country;
-    adress = myObj.results[1].data.street + " <p>" +  myObj.results[1].data.zip_code + " ," + myObj.results[1].data.place + "<p>" + myObj.results[1].data.country_code + " ," + myObj.results[1].data.country;
-    bild = myObj.results[1].data.supplier_image.url;
-    creditor = myObj.results[1].data.creditor;
-    division = myObj.results[1].data.devision;
-    partner_since_year = myObj.results[1].data.partner_since_year;
-    purchasing_volume = myObj.results[1].data.purchasing_volume;
-    estimated_leverage = myObj.results[1].data.estimated_leverage;
-    employees_female = myObj.results[1].data.emloyees_female;
-    employees_male = myObj.results[1].data.employees_male;
-    audit_type = myObj.results[1].data.audit_type;
-    fair_wear_audit = myObj.results[1].data.fair_wear_audit;
-    last_fair_wear_training = myObj.results[1].data.last_fair_wear_training;
-    bsci_id = myObj.results[1].data.bsci_id;
-    wrap_id = myObj.results[1].data.wrap_id;
-    complaints = myObj.results[1].data.complaints;
-    certificates = myObj.results[1].data.certificates;
-    mode_of_transportation = myObj.results[1].data.mode_of_transportation;
-    port_name = myObj.results[1].data.port_name;
-    port_coordinates_latitude = myObj.results[1].data.port_coordinates_latitude;
-    port_coordinates_longitude = myObj.results[1].data.port_coordinates_longitude;
-    warehouse_name = myObj.results[1].data.warehouse_name;
-    carbon_footprint = myObj.results[1].data.carbon_footprint;
+
+    json_length = Object.keys(myObj.results[1].data).length;
+
+    for(var i = 0; i < json_length; ++i){
+
+    
+    la = myObj.results[1].data[i].coordinates.latitude;
+    lo = myObj.results[1].data[i].coordinates.longitude;
+    name = myObj.results[1].data[i].name;
+    type = myObj.results[1].data[i].type;
+    street = myObj.results[1].data[i].street;
+    street_addition = myObj.results[1].data[i].street_addition;
+    zip_code = myObj.results[1].data[i].zip_code;
+    place = myObj.results[1].data[i].place;
+    country_code = myObj.results[1].data[i].country_code;
+    country = myObj.results[1].data[i].country;
+    adress = myObj.results[1].data[i].street + " <p>" +  myObj.results[1].data[i].zip_code + " ," + myObj.results[1].data[i].place + "<p>" + myObj.results[1].data[i].country_code + " ," + myObj.results[1].data[i].country;
+    bild = myObj.results[1].data[i].supplier_image.url;
+    creditor = myObj.results[1].data[i].creditor;
+    division = myObj.results[1].data[i].devision;
+    partner_since_year = myObj.results[1].data[i].partner_since_year;
+    purchasing_volume = myObj.results[1].data[i].purchasing_volume;
+    estimated_leverage = myObj.results[1].data[i].estimated_leverage;
+    employees_female = myObj.results[1].data[i].emloyees_female;
+    employees_male = myObj.results[1].data[i].employees_male;
+    audit_type = myObj.results[1].data[i].audit_type;
+    fair_wear_audit = myObj.results[1].data[i].fair_wear_audit;
+    last_fair_wear_training = myObj.results[1].data[i].last_fair_wear_training;
+    bsci_id = myObj.results[1].data[i].bsci_id;
+    wrap_id = myObj.results[1].data[i].wrap_id;
+    complaints = myObj.results[1].data[i].complaints;
+    certificates = myObj.results[1].data[i].certificates;
+    mode_of_transportation = myObj.results[1].data[i].mode_of_transportation;
+    port_name = myObj.results[1].data[i].port_name;
+    port_coordinates_latitude = myObj.results[1].data[i].port_coordinates_latitude;
+    port_coordinates_longitude = myObj.results[1].data[i].port_coordinates_longitude;
+    warehouse_name = myObj.results[1].data[i].warehouse_name;
+    carbon_footprint = myObj.results[1].data[i].carbon_footprint;
+
+    let t = new Standort(la,lo,name,type,adress,bild,creditor, division,
+      partner_since_year,purchasing_volume,estimated_leverage,employees_female,employees_male,
+      audit_type,fair_wear_audit, last_fair_wear_training,bsci_id,wrap_id,complaints,certificates,mode_of_transportation,
+      port_name,port_coordinates_latitude,port_coordinates_longitude,warehouse_name,carbon_footprint, street, street_addition,place,country_code, country);
+      
+    testo.push(t);
+
+    contentString[i]=
+    '<div id="content>' +
+    '<div id="siteNotice">' +
+    "</div>" +
+    `<h1 id="firstHeading" class="firstHeading">${ testo[i].name }</h1>` +
+    '<div id="bodyContent">' +
+    `<p><b>- Typ: </b> ${testo[i].type} </p> `+
+    `<p><b>- Adresse: </b> ${testo[i].adress} </p> `+      
+    `<p><b>- Creditor: </b> ${testo[i].creditor} </p>` + 
+    `<p><b>- Partner seit: </b> ${testo[i].partner_since_year} </p>` + 
+    `<p><b>- Purchasing volume: </b> ${testo[i].purchasing_volume} </p>` + 
+    `<p><b>- Estimated Leverage: </b> ${testo[i].estimated_leverage} </p>` + 
+    `<p><b>- Arbeiterinnen: </b> ${testo[i].employees_female} </p>` + 
+    `<p><b>- Arbeiter: </b> ${testo[i].employees_male} </p>` + 
+    `<p><b>- Beschwerden: </b> ${testo[i].complaints} </p>` + 
+    `<p><b>- Transportart: </b> ${testo[i].mode_of_transportation} </p>` + 
+    `<p><b>- CO2-Fußabdruck: </b> ${testo[i].carbon_footprint} </p>` + 
+
+   //Bild einfügen
+   `<img src=${testo[i].bild} width="400" alt="supplier_image">`
+   "</div>" +
+   "</div>";
+
+
+    }
   }
+
+ 
 };
 
 xmlhttp.open("GET", "response2.json", false);
 xmlhttp.send();
 
+/*testo = new Standort(la,lo,name,type,adress,bild,creditor, division,
+  partner_since_year,purchasing_volume,estimated_leverage,employees_female,employees_male,
+  audit_type,fair_wear_audit, last_fair_wear_training,bsci_id,wrap_id,complaints,certificates,mode_of_transportation,
+  port_name,port_coordinates_latitude,port_coordinates_longitude,warehouse_name,carbon_footprint);
+  testo.la = 3;*/
+   
 
 function initMap() {
     //hier werden alle Marker mit Standorten definiert
-    const uluru = { lat: -25.363, lng: 131.044 };
-    const jakob = {lat: 30.363, lng: 110.234};
-    const test = {lat:Number(la), lng: Number(lo)};
+   
 
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 3.5,
       center: {lat: 30.363, lng: 50.234},
     });
 
-
-    //für jeden Standort benötigen wir den Inhalt, der bei einem Klick
-    //auf den Marker angezeigt werden soll (Text + Bild)
-    const contentString =
-      '<div id="content">' +
-      '<div id="siteNotice">' +
-      "</div>" +
-      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
-      '<div id="bodyContent">' +
-      "<p><b>T-Rex</b>, also referred to as <b>Ayers Rock</b>, is a large " +
-      "sandstone rock formation in the southern part of the " +
-      "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
-      "south west of the nearest large town, Alice Springs; 450&#160;km " +
-      "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
-      "features of the Uluru - Kata Tjuta National Park. Uluru is " +
-      "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
-      "Aboriginal people of the area. It has many springs, waterholes, " +
-      "rock caves and ancient paintings. Uluru is listed as a World " +
-      "Heritage Site.</p>" +
-      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
-      "https://de.wikipedia.org/wiki/Dinosaurier</a> " +
-      "(last visited May 16, 2021).</p>" +
-      //Bild einfügen
-      '<img src="https://www.ntz.de/uploads/tx_templavoila/nzwz-a67d6045-8644-4e40-9536-32a67edada4e_onlineBild.jpg" width="400" alt="großer Dino">'
-      "</div>" +
-      "</div>";
-
-      //Standort 2
-      const contentString2 =
-      '<div id="content">' +
-      '<div id="siteNotice">' +
-      "</div>" +
-      '<h1 id="firstHeading" class="firstHeading">Hier gibts keine Rechte </h1>' +
-      '<div id="bodyContent">' +
-      "<p><b>Test1</b>, ist  <b>Text</b>, drinnen " +
-      "test2" +
-      //Bild einfügen
-      '<img src="https://www.tagesspiegel.de/images/google-logo/12266780/4-format43.jpg" width="400" alt="google">'
-      "</div>" +
-      "</div>";
-
-      const contentString3 =
-      '<div id="content>' +
-      '<div id="siteNotice">' +
-      "</div>" +
-      `<h1 id="firstHeading" class="firstHeading">${ name }</h1>` +
-      '<div id="bodyContent">' +
-      `<p><b>- Typ: </b> ${type} </p> `+
-      `<p><b>- Adresse: </b> ${adress} </p> `+      
-      `<p><b>- Creditor: </b> ${creditor} </p>` + 
-      `<p><b>- Partner seit: </b> ${partner_since_year} </p>` + 
-      `<p><b>- Purchasing volume: </b> ${purchasing_volume} </p>` + 
-      `<p><b>- Estimated Leverage: </b> ${estimated_leverage} </p>` + 
-      `<p><b>- Arbeiterinnen: </b> ${employees_female} </p>` + 
-      `<p><b>- Arbeiter: </b> ${employees_male} </p>` + 
-      `<p><b>- Beschwerden: </b> ${complaints} </p>` + 
-      `<p><b>- Transportart: </b> ${mode_of_transportation} </p>` + 
-      `<p><b>- CO2-Fußabdruck: </b> ${carbon_footprint} </p>` + 
-
-     //Bild einfügen
-     `<img src=${bild} width="400" alt="supplier_image">`
-     "</div>" +
-     "</div>";
-
-    //es wird nur ein infowindow benötigt
     const infowindow = new google.maps.InfoWindow({
     });
 
     //für jeden Standort benötigen wir einen Marker
-    const marker = new google.maps.Marker({
-      position: uluru,
-      map,
-      title: "Uluru (Ayers Rock)",
-    });
-
-    
-
-    const marker2 = new google.maps.Marker({
-        position: jakob,
-        map,
-        title: "Jakob (hart wie ein Stein)",
-    });
-
-    const marker3 = new google.maps.Marker({
-      position: test,
-      map,
-      title: "Test",
-    });
-    
-    marker3.category = type;
-    markers.push(marker3);
-
-    
-
-    marker.addListener("click", () => {
-      infowindow.setContent(contentString);
-      infowindow.open(map, marker);
-    });
-
-    marker2.addListener("click", () => {
-        infowindow.setContent(contentString2);
-        infowindow.open(map, marker2);
-        map.setZoom(8);
-        map.setCenter(marker2.getPosition());
-      });
-
-      marker3.addListener("click", () => {
-        infowindow.setContent(contentString3);
-        infowindow.open(map, marker3);
-        map.setZoom(8);
-        map.setCenter(marker3.getPosition());
-      });
+   
 
       //Test mit Marker-Feld
 
-      var coord = [
-        [12.213, 33.123],
-        [33.999, 25.23],
-        [-33.444, 60.73]
-      ];
-
-      var ti = ["Eins", "Zwei", "Drei"];
-
-      var ty = ["Factory", "Supplier", "Supplier"];
-
-      var n = ["Eins", "Zwei", "Drei"];
-
-      for (i = 0; i < 3; i++) {
+      for (i = 0; i < json_length; i++) {
         newMarker = new google.maps.Marker({
-          position: {lat: coord[i][0], lng: coord[i][1]},
+          position: {lat: testo[i].getLa(), lng: testo[i].getLo()},
           map: map,
-          title: ti[i]
+          title: testo[i].name
         });
 
-        newMarker.category = ty[i];
+        newMarker.category = testo[i].type;
 
         markers2.push(newMarker);
       }
@@ -231,7 +213,7 @@ function initMap() {
       for(i = 0; i < markers2.length; i++) {
         const a = i;
         markers2[a].addListener("click", () => {
-          infowindow.setContent("Test" + (a+1));
+          infowindow.setContent(contentString[a] );
           infowindow.open(map, markers2[a]);
           map.setZoom(8);
           map.setCenter(markers2[a].getPosition());
@@ -269,22 +251,12 @@ function initMap() {
         var i;
 
         if(this.checked) {
-          for (i = 0; i < markers.length; i++) {
-            if(markers[i].category === "Factory") {
-              markers[i].setVisible(true);
-            }   
-          }
           for (i = 0; i < markers2.length; i++) {
             if(markers2[i].category === "Factory") {
               markers2[i].setVisible(true);
             } 
           }
         } else {
-          for (i = 0; i < markers.length; i++) {
-            if(markers[i].category === "Factory") {
-              markers[i].setVisible(false);
-            } 
-          }
           for (i = 0; i < markers2.length; i++) {
             if(markers2[i].category === "Factory") {
               markers2[i].setVisible(false);
@@ -303,62 +275,43 @@ function initMap() {
         var i;
 
         if(this.checked) {
-          for (i = 0; i < markers.length; i++) {
-            if(markers[i].category === "Supplier") { //Wir wissen noch nicht wie ein Lieferant in der Datenbank heißt, deshalb mal so bis jetzt
-              markers[i].setVisible(true);
-            } 
-          }
           for (i = 0; i < markers2.length; i++) {
             if(markers2[i].category === "Supplier") {
               markers2[i].setVisible(true);
             } 
           }
         } else {
-          for (i = 0; i < markers.length; i++) {
-            if(markers[i].category === "Supplier") { //Wir wissen noch nicht wie ein Lieferant in der Datenbank heißt, deshalb mal so bis jetzt
-              markers[i].setVisible(false);
-            } 
             for (i = 0; i < markers2.length; i++) {
               if(markers2[i].category === "Supplier") {
                 markers2[i].setVisible(false);
               } 
             }
         }
-      }
-    });
+      });
 
     var button = document.getElementById('button');
-
+      // Hier wird die Funktionalität der Suchleiste implementiert
+      
     button.addEventListener("click", function() {
       var i;
       var input = document.getElementById('searchbar').value.toLowerCase();
-      
       var eingabe = false;
 
       for(i = 0; i < markers2.length; i++) {
         markers2[i].setVisible(false);
-        if(n[i].toLowerCase() == input) {
+        if(testo[i].name.toLowerCase() == input || testo[i].country.toLowerCase() == input || testo[i].zip_code == input) {
           markers2[i].setVisible(true);
           eingabe = true;
         }
-
+  
       }
-      for(i = 0; i < markers.length; i++) {
-        markers[i].setVisible(false);
-        if(name.toLowerCase() == input.toLowerCase() || country.toLowerCase() == input.toLowerCase() || place.toLowerCase() == input.toLowerCase() || zip_code == input) {
-          markers[i].setVisible(true);
-          eingabe = true;
-        }
-      }
-
+  
       if (eingabe == false) {
-        for (i = 0; i < markers.length; i++) {
-          markers[i].setVisible(true);
-        }
         for(i = 0; i < markers2.length; i++) {
           markers2[i].setVisible(true);
         }
       }
+      
       
     });
 
