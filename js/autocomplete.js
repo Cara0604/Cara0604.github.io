f271655257782d3761e51f6b0d39e1f30912ee5f
steel809 <77510251+steel809@users.noreply.github.com>
2021-06-20 23:01:39 +0200

    Kommentare sind drin, Code wurde aufgeräumt Bilder gelöscht Datenstruktur eingebaut css besser strukturiert code aufgehübscht => Wichtig!! Autocomplet funktionsweise verstehen

--- js/autocomplete.js
+++ js/autocomplete.js
@@ -1,119 +1,108 @@
-//Hier wird der Input und die Datei übergeben */
+//Hier wird der Input und die Datei übergeben
 function autocomplete(inp, arr) {
-    
-    var currentFocus;
-    /*Funktion wird ausgelöst, wenn Eingabe gestartet wird*/
-    inp.addEventListener("input", function (e) {
-        var a, b, i, val = this.value; //value von input hinzugefügt
-        closeAllLists();
 
-        if (!val) { return false; }
-        currentFocus = -1;
+	var currentFocus;
+	//Funktion wird ausgelöst, wenn Eingabe gestartet wird
+	inp.addEventListener("input", function(e) {
+		var a, b, i, val = this.value; //value von input hinzugefügt
+		closeAllLists();
 
-        /*Erstellt ein div Element aus dem Datensatz*/
-        a = document.createElement("DIV");
-        a.setAttribute("id", this.id + "autocomplete-list");
-        a.setAttribute("class", "autocomplete-items");
-        /*append the DIV element as a child of the autocomplete container:*/
-        this.parentNode.appendChild(a);
-        /*for each item in the array...*/
-        for (i = 0; i < arr.length; i++) {
-            /*check if the item starts with the same letters as the text field value:*/
-            if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-                /*create a DIV element for each matching element:*/
-                b = document.createElement("DIV");
-                /*make the matching letters bold:*/
-                b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-                b.innerHTML += arr[i].substr(val.length);
-                /*insert a input field that will hold the current array item's value:*/
-                b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-                /*execute a function when someone clicks on the item value (DIV element):*/
-                b.addEventListener("click", function (e) {
-                    /*insert the value for the autocomplete text field:*/
-                    inp.value = this.getElementsByTagName("input")[0].value;
-                    /*close the list of autocompleted values,
-                    (or any other open lists of autocompleted values:*/
-                    closeAllLists();
-                    //triggers the search button
-                    $('#button').trigger('click');
-                });
-                a.appendChild(b);
-            }
-        }
-    });
-    /*Keyboardsteuerung innerhalb der Liste mit den Pfeiltasten*/
-    inp.addEventListener("keydown", function (e) {
-        var x = document.getElementById(this.id + "autocomplete-list");
-        if (x) x = x.getElementsByTagName("div");
-        if (e.keyCode == 40) { //down
-            /*Wenn Pfeiltaste nach unten gedrückt wird, dann geh runter*/
-            currentFocus++;
-            /*Zeig das ausgewählt visuell besser*/
-            addActive(x);
-        } else if (e.keyCode == 38) { //up
-            /*Wenn Pfeiltaste nach oben gedrückt wird, dann geh hoch*/
-            currentFocus--;
-            /*Zeig das ausgewählt visuell besser*/
-            addActive(x);
-        } else if(e.keyCode == 13) { //enter Funktion einbauen
-            if(currentFocus > -1) {
-                if(x) {
-                    x[currentFocus].click();
-                }
-            }
-            $("#searchForm").submit(function () {
-                document.getElementById('button').click();
-                return false;
-            });
+		if (!val) {
+			return false;
+		}
+		currentFocus = -1;
 
-            //womöglich sind diese unnötig geworden
-            // $('.info').removeClass("show");
-            // $('.info').addClass("hide");
-            // setTimeout(() => {  $('.info').removeClass("showInfo"); }, 1100);
-            // $('.container').removeClass("active");
-            
-        }
-    })
-    function addActive(x) {
-        /*a function to classify an item as "active":*/
-        if (!x) return false;
-        /*start by removing the "active" class on all items:*/
-        removeActive(x);
-        if (currentFocus >= x.length) currentFocus = 0;
-        if (currentFocus < 0) currentFocus = (x.length - 1);
-        /*add class "autocomplete-active":*/
-        x[currentFocus].classList.add("autocomplete-active");
-        
-    }
-    function removeActive(x) {
-        /*a function to remove the "active" class from all autocomplete items:*/
-        for (var i = 0; i < x.length; i++) {
-            x[i].classList.remove("autocomplete-active");
-        }
-        
-    }
-    function closeAllLists(elmnt) {
-        /*close all autocomplete lists in the document,
-        except the one passed as an argument:*/
-        var x = document.getElementsByClassName("autocomplete-items");
-        for (var i = 0; i < x.length; i++) {
-            if (elmnt != x[i] && elmnt != inp) {
-                x[i].parentNode.removeChild(x[i]);
-            }
-        }
-        
-    }
-    /*execute a function when someone clicks in the document:*/
-    document.addEventListener("click", function (e) {
-        closeAllLists(e.target);
-    });
+		//Erstellt ein div Element aus dem Datensatz
+		a = document.createElement("DIV");
+		a.setAttribute("id", this.id + "autocomplete-list");
+		a.setAttribute("class", "autocomplete-items");
+		//Dieser wird als 'Kind vom ganzen Autocomplete-Container geestellt:
+		this.parentNode.appendChild(a);
+		//Für jedes Element im übergebenen Feld arr...
+		for (i = 0; i < arr.length; i++) {
+			// Wird überprüft, ob das eingebene Element in die Suchleiste mit den selben Buchstaben beginnt, wie die elemente im Feld
+			if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+				// Für jeden Treffer wird ein eigenes Div erstellt: 
+				b = document.createElement("DIV");
+				// Die Buchstaben, die sich übereinstimmen, werden fett gemacht
+				b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+				b.innerHTML += arr[i].substr(val.length);
+				//verstehe ich noch nict
+				/*insert a input field that will hold the current array item's value:*/
+				b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+				//hier wird ein Listener hinzugefügt, falls jemand auf ein Element in der Liste klickt
+				b.addEventListener("click", function(e) {
+					//erst wird der Text in das Suchfeld übernommen:
+					inp.value = this.getElementsByTagName("input")[0].value;
+					//anschließend wird die komplette Liste geschlossen: 
+					closeAllLists();
+					//löst dann den Suchbutton aus, wie als hätte man darauf geklickt
+					$('#button').trigger('click');
+				});
+				a.appendChild(b);
+			}
+		}
+	});
+	/*Keyboardsteuerung innerhalb der Liste mit den Pfeiltasten*/
+	inp.addEventListener("keydown", function(e) {
+		var x = document.getElementById(this.id + "autocomplete-list");
+		if (x) x = x.getElementsByTagName("div");
+		if (e.keyCode == 40) { //down
+			/*Wenn Pfeiltaste nach unten gedrückt wird, dann geh runter*/
+			currentFocus++;
+			/*Zeig das ausgewählt visuell besser an*/
+			addActive(x);
+		} else if (e.keyCode == 38) { //up
+			/*Wenn Pfeiltaste nach oben gedrückt wird, dann geh hoch*/
+			currentFocus--;
+			/*Zeig das ausgewählt visuell besser*/
+			addActive(x);
+		} else if (e.keyCode == 13) { //enter Funktion einbauen
+			if (currentFocus > -1) {
+				if (x) {
+					x[currentFocus].click();
+				}
+			}
+			$("#searchForm").submit(function() {
+				document.getElementById('button').click();
+				return false;
+			});
+		}
+	})
 
-    var button = document.querySelector('.search');
-    button.addEventListener("click", () => {
-        $('.info').removeClass("show");
-        $('.info').addClass("hide");
-        setTimeout(() => {  $('.info').removeClass("showInfo"); }, 1100);
-        $('.container').removeClass("active");
+	function addActive(x) {
+		//hiermit wird ein element die Klasse "autocomplete-active zugewiesen"
+		if (!x)
+			return false;
+		//anschließend entfernt man bei allen anderen Elemente "active"
+		removeActive(x);
+		if (currentFocus >= x.length) currentFocus = 0;
+		if (currentFocus < 0) currentFocus = (x.length - 1);
+		//hier wird die klasse hinzugefügt
+		x[currentFocus].classList.add("autocomplete-active");
 
-    });
+	}
+
+	function removeActive(x) {
+		//function um "active" von allen anderen autocomplete elementen zu entfernen
+		for (var i = 0; i < x.length; i++) {
+			x[i].classList.remove("autocomplete-active");
+		}
+
+	}
+
+	function closeAllLists(elmnt) {
+		//es schließt die gesamte Liste, außer das übergeben elmnt
+		var x = document.getElementsByClassName("autocomplete-items");
+		for (var i = 0; i < x.length; i++) {
+			if (elmnt != x[i] && elmnt != inp) {
+				x[i].parentNode.removeChild(x[i]);
+			}
+		}
+
+	}
+	//sobald jemand auf die Webseite clickt, wird die Liste geschlossen
+	document.addEventListener("click", function(e) {
+		closeAllLists(e.target);
+	});
 }
---
+++ js/data.js
@@ -0,0 +1,52 @@
+var street;
+var zip_code;
+var place;
+var country;
+var name;
+var infos = [];
+var json_length;
+
+var url = 'https://schoeffel-b2c.cdn.prismic.io/api/v2/documents/search?ref=YMnpZxMAACoACiwF';
+
+// Die Daten werden in verschieden Websiten, damit in verschiedene Links gespeichert. Um an sie ranzukommen muss man hierfür, die variable url austauschen mit dem nächsten Link. Die wird in dem Attribut myObj.next_page abgerufen
+
+while (url != null) {
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) { //Abfrage hat funktioniert
+			var myObj = JSON.parse(this.responseText);
+
+			url = myObj.next_page;
+
+			json_length = Object.keys(myObj.results).length;
+
+			//falls der Inhalt der Attributes nicht leer ist, wird er dem Feld infos hinzugefügt
+			for (var i = 0; i < json_length; ++i) {
+				if (myObj.results[i].data.country != null) {
+					infos.push(myObj.results[i].data.country);
+				}
+				if (myObj.results[i].data.place != null) {
+					infos.push(myObj.results[i].data.place);
+				}
+				if (myObj.results[i].data.name != null) {
+					infos.push(myObj.results[i].data.name);
+				}
+			}
+
+			//sollte etwas doppelt sein, wird es hier gelöscht
+			for (var i = 0; i < infos.length; i++) {
+				for (var j = 0; j < infos.length; j++) {
+					if ((infos[i] == infos[j]) && i != j) {
+						infos.splice(j, 1);
+					}
+				}
+			}
+		}
+	};
+
+	//holt sich die Daten
+	xmlhttp.open("GET", url, false);
+	xmlhttp.send();
+}
+//hiermit übergeben wir Daten an autocomplete.js, um die Autcomplete funktion zu ermöglichen. Hier wird die Eingabe und unsere Variable mit allen Straßen, Plätzen und Ländern übergeben
+autocomplete(document.querySelector('.myInput'), infos);
